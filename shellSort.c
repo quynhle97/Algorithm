d9871582890e8de90e6980955ccec4300c75144a
quynhle97 <quynhle13301@gmail.com>
2018-02-21 22:03:10 +0700

    Shell sort algorithm

---
+++ shellSort.c
@@ -0,0 +1,62 @@
+void swap(int *a, int *b) {
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void shellSort1(int a[], int N) {
+    int gap, i, j;
+    for (gap = N/2; gap>0; gap/=2) {
+        for (i=gap; i<N; i++) {
+            for (j=i-gap; j>=0 && a[j]>a[j+gap]; j-=gap) {
+                swap(&a[j], &a[j+gap]);
+                xuatmang(a, N);
+            }
+        }
+    }
+}
+
+void shellSort2(int a[], int N) {
+    int x, j;
+    for (int gap=N/2; gap>0; gap/=2) {
+        for (int i=gap; i<N; i++) {
+            x = a[i];
+            j = i-gap;
+            while ((x<a[j]) && (j>=0)) {
+                a[j+gap] = a[j];
+                j = j - gap;
+
+            }
+            a[j+gap] = x;
+            xuatmang(a, N);
+        }
+    }
+}
+
+void shellSort3(int a[], int N, int h[], int k) {
+    int x,j, gap;
+    for (int step=0; step<k; step++) {
+        gap = h[step];
+        for (int i=gap; i<N; i++) {
+            x=a[i];
+            j=i-gap;
+            while ((x<a[j]) && (j>=0)) {
+                a[j+gap] = a[j];
+                j=j-gap;
+            }
+            a[j+gap] = x;
+            xuatmang(a, N);
+        }
+    }
+}
+
+void bubbleSort1(int a[], int N) {
+    for (int i=0; i<N-1; i++) {
+        for (int j=0; j<N-i-1; j++) {
+            if (a[j]>a[j+1]) {
+                swap(&a[j], &a[j+1]);
+                xuatmang(a, N);
+            }
+        }
+    }
+}
