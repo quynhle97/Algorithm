7fbef8b0f6937cd00bac7c26004c10b037409f19
quynhle97 <35551956+quynhle97@users.noreply.github.com>
2018-02-21 22:05:24 +0700

    Delete lines that are abundant.

--- shellSort.c
+++ shellSort.c
@@ -49,14 +49,3 @@ void shellSort3(int a[], int N, int h[], int k) {
         }
     }
 }
-
-void bubbleSort1(int a[], int N) {
-    for (int i=0; i<N-1; i++) {
-        for (int j=0; j<N-i-1; j++) {
-            if (a[j]>a[j+1]) {
-                swap(&a[j], &a[j+1]);
-                xuatmang(a, N);
-            }
-        }
-    }
-}
