2e3628ec9e3d0a29f308c135c5ce5465a2d0d1e4
quynhle97 <quynhle13301@gmail.com>
2018-02-23 21:43:32 +0700

    Quick sort function

--- quickSort.c
+++ quickSort.c
@@ -1,22 +1,4 @@
-#include <stdio.h>
-
-void swap(int *a, int *b);
-void xuatmang(int a[], int N);
-void quickSort(int a[], int left, int right);
-
-int main() {
-    int a[] = {23, 45, 71, 11, 1, 92, 190, 11, 0, 23, 2};
-    int N = sizeof (a)/sizeof (a[0]);
-    quickSort(a, 0, N-1);
-    xuatmang(a, N);
-    return 0;
-}
-
-void swap(int *a, int *b) {
-    int c = *a;
-    *a = *b;
-    *b = c;
-}
+#include "main.h"
 
 void quickSort(int a[], int left, int right) {
     int mid = a[(left+right)/2];
@@ -32,16 +14,4 @@ void quickSort(int a[], int left, int right) {
     } while (i<j);
     if (left<j) quickSort(a,left,j);
     if (i<right) quickSort(a,i, right);
-}
-
-void xuatmang(int a[], int N) {
-    printf("-");
-    for (int i=0; i<N; i++) {
-        printf("%6d",i);
-    }
-    printf("\n ");
-    for (int i=0; i<N; i++) {
-        printf("%6d", a[i]);
-    }
-    printf("\n");
 }
