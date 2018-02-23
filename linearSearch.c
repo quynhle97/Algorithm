03a0de540160a20000fafc1ab0d150ac66540bcc
quynhle97 <35551956+quynhle97@users.noreply.github.com>
2018-02-23 19:53:21 +0700

    Linear search
    Fix errror

--- linearSearch.c
+++ linearSearch.c
@@ -1,6 +1,22 @@
-void linearSearch (int a[], int N, int x) {
-	int i=0;
-	while (a[i] != x && i<N) i++;
-	if (i==N) return 0;
-	else return 1;
+#include <stdio.h>
+
+int linearSearch (int a[], int N, int x);
+
+int main() {
+    int a[] = {23,45,71,1010,21,1,2,103,57};
+    int N = sizeof (a)/sizeof (a[0]);
+    int x = 21, pos;
+
+    if ((pos = linearSearch(a, N, x)) != -1)
+        printf("Co %d trong mang o vi tri %d\n", x, pos);
+    else printf("Khong tim thay\n");
+
+    return 0;
+}
+
+int linearSearch (int a[], int N, int x) {
+    int i=0;
+    while (a[i] != x && i<N) i++;
+    if (i==N) return -1;
+    else return i;
 }
