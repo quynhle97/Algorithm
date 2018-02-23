73005188c07709a6e53738ed105b79a3287b50b9
quynhle97 <quynhle13301@gmail.com>
2018-02-23 21:44:56 +0700

    Selection sort function

--- selectionSort.c
+++ selectionSort.c
@@ -1,43 +1,12 @@
-#include <stdio.h>
-
-void swap(int *a, int *b);
-void xuatmang(int a[], int N);
-void selectionSort(int a[], int N);
-
-int main() {
-    int a[] = {23,45,71,1010,21,21,1,2,103,57};
-    int N = sizeof (a)/sizeof (a[0]);
-    selectionSort(a, N);
-
-    xuatmang(a,N);
-    return 0;
-}
+#include "main.h"
 
 void selectionSort(int a[], int N) {
-    int pos;
+    int posmin;
     for (int i=0; i<N; i++) {
-        pos = i;
+        posmin = i;
         for (int j=i; j<N; j++) {
-            if (a[j] < a[pos]) pos = j;
+            if (a[j] > a[posmin]) posmin = j;
         }
-        swap(&a[pos], &a[i]);
+        swap(&a[posmin], &a[i]);
     }
-}
-
-void swap(int *a, int *b) {
-    int c = *a;
-    *a = *b;
-    *b = c;
-}
-
-void xuatmang(int a[], int N) {
-    printf("-");
-    for (int i=0; i<N; i++) {
-        printf("%6d",i);
-    }
-    printf("\n ");
-    for (int i=0; i<N; i++) {
-        printf("%6d", a[i]);
-    }
-    printf("\n");
 }
