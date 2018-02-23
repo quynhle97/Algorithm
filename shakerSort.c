6e539724eb2d3ce36aa6a6a08ba25218c2a4c62b
quynhle97 <quynhle13301@gmail.com>
2018-02-23 21:47:17 +0700

    Shaker sort function

--- shakerSort.c
+++ shakerSort.c
@@ -1,24 +1,6 @@
-#include <stdio.h>
+#include "main.h"
 
-void swap(int *a, int *b);
-void xuatmang(int a[], int N);
-void shakerSort (int a[], int N);
-
-int main() {
-    int a[] = {90,323,34,1,23,123,234,11,0,10};
-    int N = sizeof (a)/sizeof (a[0]);
-
-    shakerSort(a,N);
-    xuatmang(a, N);
-    return 0;
-}
-
-void swap(int *a, int *b) {
-    int c = *a;
-    *a = *b;
-    *b = c;
-}
-
+// Bien k la mot bien rat thu vi
 // k dong vai tro la bien tam, luu giu gia tri cua lan swap cuoi cung sau vong for, roi thay gia tri left, right cho cac lan tiep theo
 // Bien left va right la 2 cot moc de danh dau vi tri tiep theo de sap xep [left..right]
 
@@ -51,15 +33,3 @@ void shakerSort (int a[], int N) {
 
 // Khac bubbleSort o cho la bubbleSort khong nhan dien day da co thu tu hay co thu tu tung phan
 // Con shakerSort thi biet va gioi han lai khoang can sap xep
-
-void xuatmang(int a[], int N) {
-    printf("-");
-    for (int i=0; i<N; i++) {
-        printf("%6d",i);
-    }
-    printf("\n ");
-    for (int i=0; i<N; i++) {
-        printf("%6d", a[i]);
-    }
-    printf("\n");
-}
