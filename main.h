2b49596569b97184711dfea70752d44a4e264c64
quynhle97 <quynhle13301@gmail.com>
2018-02-23 22:00:17 +0700

    Main.h header file

---
+++ main.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+void xuatmang(int a[], int N);
+
+int linearSearch (int a[], int N, int x);
+int binarySearch(int a[], int N, int x);
+
+void bubbleSort1(int a[], int N);
+void interchangeSort(int a[], int N);
+void shakerSort (int a[], int N);
+void selectionSort(int a[], int N);
+void selectionSort_improve(int a[], int N);
+void insertionSort(int a[], int N);
+void shellSort1(int a[], int N);
+void shellSort2(int a[], int N);
+void shellSort3(int a[], int N, int h[], int k);
+void radixSort(int a[], int N);
+void quickSort(int a[], int left, int right);
