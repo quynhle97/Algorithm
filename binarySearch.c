7a939ce83d8705fe5958c0a8d77c7dde9e2131e6
quynhle97 <quynhle13301@gmail.com>
2018-02-23 21:40:40 +0700

    Binary search function

--- binarySearch.c
+++ binarySearch.c
@@ -1,29 +1,16 @@
-#include <stdio.h>
-
-int binarySearch(int a[], int N, int x);
-
-int main() {
-    int a[] = {23,45,71,1010,21,21,1,2,103,57};
-    int N = sizeof (a)/sizeof (a[0]);
-    int x = 21;
-
-    if (binarySearch(a, N, x) != -1)
-        printf("Co %d trong mang\n", x);
-    else printf("Khong tim thay\n");
-
-    return 0;
-}
+#include "main.h"
 
 int binarySearch(int a[], int N, int x) {
     int mid;
     int left = 0, right = N-1;
     do {
         mid = (left + right)/2;
-        if (a[mid] == x) return 1;
+        if (a[mid] == x) return mid;
         else {
             if (a[mid]<x) left = mid + 1;
             else right = mid - 1;
         }
     } while (left <= right);
-    return -1;
+    return 0;
 }
+
