0596f5de0379c36132523794bb90570a5e2feaae
quynhle97 <35551956+quynhle97@users.noreply.github.com>
2018-02-23 19:45:12 +0700

    Improved selection sort algorithm
    Changed better than previous version

--- selectionSort_improve.c
+++ selectionSort_improve.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+void xuatmang(int a[], int N);
+void selectionSort(int a[], int N);
+
+int main() {
+    int a[] = {23,45,71,1010,21,21,1,2,103,57};
+    int N = sizeof (a)/sizeof (a[0]);
+    selectionSort(a, N);
+
+    xuatmang(a,N);
+    return 0;
+}
+
+
 void selectionSort(int a[], int N) {
     int posmin, posmax;
     for (int i=0; i<N/2; i++) {
@@ -8,7 +24,9 @@ void selectionSort(int a[], int N) {
             if (a[j] > a[posmax]) posmax = j;
         }
         swap(&a[posmin], &a[i]);
+        //xuatmang(a, N);
         swap(&a[posmax], &a[N-1-i]);
+        //xuatmang(a, N);
     }
 }
 
@@ -16,4 +34,16 @@ void swap(int *a, int *b) {
     int c = *a;
     *a = *b;
     *b = c;
+}
+
+void xuatmang(int a[], int N) {
+    printf("-");
+    for (int i=0; i<N; i++) {
+        printf("%6d",i);
+    }
+    printf("\n ");
+    for (int i=0; i<N; i++) {
+        printf("%6d", a[i]);
+    }
+    printf("\n");
 }
