2f5149a5b3c49c59da263b4586da9d8ed38bf6a5
quynhle97 <quynhle13301@gmail.com>
2018-02-23 21:42:16 +0700

    Insertion sort function

--- insertionSort.c
+++ insertionSort.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
-void swap(int *x, int *y);
-void xuatmang(int a[], int n);
-
-int main()
-{
-    int a[] = {3, 1, 9, 7, 2, 109, 7, 2, 10, 4};
-    int len = sizeof(a)/sizeof(int);
-
-    for (int i = 0; i < len; i++) {
+void insertionSort(int a[], int N) {
+    for (int i = 0; i < N; i++) {
         for (int j = 0; j < i; j++) {
             if (a[j] >= a[i]) {
                 int tmp = a[i];
@@ -18,24 +11,8 @@ int main()
                     n++;
                 }
                 a[j] = tmp;
-                break; 
+                break;
             }
         }
     }
-    xuatmang(a, len);
-    return 0;
-}
-
-void swap(int *x, int *y) {
-    int c;
-    c = *x;
-    *x = *y;
-    *y = c;
-}
-
-void xuatmang(int a[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
 }
