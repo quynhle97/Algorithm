4388d17f69ebc8938c977b5de27a5f97c0093c31
quynhle97 <quynhle13301@gmail.com>
2018-03-02 19:29:25 +0700

    main.h

--- main.h
+++ main.h
@@ -7,15 +7,26 @@ int linearSearch (int a[], int N, int x);
 int binarySearch(int a[], int N, int x);
 
 void bubbleSort1(int a[], int N);
+
 void interchangeSort(int a[], int N);
-void shakerSort (int a[], int N);
+
 void selectionSort(int a[], int N);
 void selectionSort_improve(int a[], int N);
+
+void shakerSort (int a[], int N);
+
 void insertionSort(int a[], int N);
+
 void shellSort1(int a[], int N);
 void shellSort2(int a[], int N);
 void shellSort3(int a[], int N, int h[], int k);
+
 void radixSort(int a[], int N);
+
 void quickSort(int a[], int left, int right);
+
 void mergeSort(int a[], int l, int r);
 void merge(int a[], int l, int r, int m);
+
+void heapSort(int a[], int N);
+void heap(int a[], int N, int tmp);
