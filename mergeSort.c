fcc52ab9e2757ef8ff5128b2d00f0b89f569deb7
quynhle97 <quynhle13301@gmail.com>
2018-03-02 19:29:46 +0700

    Merge sort algorithm

--- mergeSort.c
+++ mergeSort.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include "main.h"
 
 // ham "merge" : hop nhat cac mang con va luu vao lai mang chinh
 // ham "mergeSort" : phan chia mang thanh cac mang con, 'sap xep' (la so sanh roi chon ra phan tu thich hop) va hop nhat cac mang con do vao mang chinh
@@ -17,45 +17,47 @@ void mergeSort(int a[], int l, int r) {
 }
 
 void merge(int a[], int l, int r, int m) {
-   // Bien "lup" : so phan ben mang con ben trai
-   int lup = m - l + 1;
-   // Bien "rup" : so phan ben mang con ben phai
-   int rup = r - m;
+    // Bien "lup" : so phan ben mang con ben trai
+    int lup = m - l + 1;
+    // Bien "rup" : so phan ben mang con ben phai
+    int rup = r - m;
 
-   int left[lup], right[rup];
+    int left[lup], right[rup];
 
-   for (int i=0; i<lup; i++)
-       left[i] = a[l+i];
+    for (int i=0; i<lup; i++) {
+        left[i] = a[l+i];
+    }
 
-   for (int i=0; i<rup; i++)
-       right[i] = a[m+1+i];
 
-   int k=l; // Bien k: bien vi tri de luu gia tri trong mang a, k = left chu k khong phai = 0;
-   int i=0, j=0;
-   // Luu vao mang chinh
-   // khi 2 ben mang con con gia tri thi tiep tuc so sanh va luu gia tri vao mang a
-   while (i<lup && j<rup) {
-       if (left[i] < right[j]) {
-           a[k] = left[i];
-           i++;
-       } else {
-           a[k] = right[j];
-           j++;
-       }
-       k++;
-   }
+    for (int i=0; i<rup; i++)
+        right[i] = a[m+1+i];
 
-   // doc gia tri con lai cua mang con ma con phan tu
-   
-   while (i<lup) {
-       a[k] = left[i];
-       i++;
-       k++;
-   }
+    int k=l; // Bien k: bien vi tri de luu gia tri trong mang a, k = left chu k khong phai = 0;
+    int i=0, j=0;
+    // Luu vao mang chinh
+    // khi 2 ben mang con con gia tri thi tiep tuc so sanh va luu gia tri vao mang a
+    while (i<lup && j<rup) {
+        if (left[i] < right[j]) {
+            a[k] = left[i];
+            i++;
+        } else {
+            a[k] = right[j];
+            j++;
+        }
+        k++;
+    }
 
-   while (j<rup) {
-       a[k] = right[j];
-       j++;
-       k++;
-   }
+    // doc gia tri con lai cua mang con ma con phan tu
+
+    while (i<lup) {
+        a[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j<rup) {
+        a[k] = right[j];
+        j++;
+        k++;
+    }
 }
